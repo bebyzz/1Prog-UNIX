78c9175b7bba519be045cc93626486efff97c563
SIeviN <mdk54@txstate.edu>
2016-02-26 23:47:02 -0600

    Added files via upload
    added line verification

---
+++ delete.sh
@@ -0,0 +1,45 @@
+#/bin/bash
+#find and delete from text based database
+#assuming $1 is the string we are trying to find
+#CASE SENSITIVE, deletes entire contents otherwise
+
+if [ $# -ne 1 ]
+then
+	echo "Must enter only one argument"
+	exit 1
+fi
+
+#test if string is in file
+grep -q "$1" db.dat
+if [ $? -eq 0 ]
+#if string is found
+then
+	#find all instances of the string to be deleted	
+	grep -n "$1" db.dat
+	echo "Enter the line number of the entry you would like to delete"
+	read line
+	sed -n "$line p" "db.dat" > match.txt
+	grep -q "$1" match.txt
+	rm -f match.txt
+	if [ $? -ne 0 ]
+	then
+		echo "Line number entered does not match matched lines."
+		exit 1
+	else 
+		#remove $line from the database file
+		sed -i "$line d" "db.dat"
+		echo "Entry removed from database."
+		exit 0
+	fi
+else
+#if string is not found in file
+	echo "Not found in database."
+	exit 1
+fi
+
+
+#function deleteempty
+#{
+	#delete empty lines in database
+	#sed -n '/^$/d' db.dat
+#}
