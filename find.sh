b09fe7f5f88310655684f2bf003aa0f7f4002360
Yinebeb Zenaw <yinebebz@gmail.com>
2016-02-28 01:47:10 -0600

    fixed a bug where an extra character was keeping     The search from working

--- find.sh
+++ find.sh
@@ -6,70 +6,62 @@
 if [ $# -lt 2 ]; then
 	echo "Too few argumetns passed to "$0" "  1>&2
 	exit 1
-else
-	for i in $@;do
-		#$1 is the search colum >$2 are search terms
-		if [ $i == $1 ];then 
-			continue
-		fi
-		search=$search" "$i
-	done
 fi
 
-function getuserid 
-{
-	records=$(cut -d: -f1 db.dat | grep -n "$search" | cut -d: -f1)
-	#for debuging
-	#echo $recordsi
-}
 
+search=""
+for i in $@;do
+	#$1 is the search colum >$2 are search terms
+	if [ $i == $1 ];then 
+		continue
+	fi
+	search=$search" "$i
+done
+#search has a blank space add to the beging used to remove that inital blank
+search="${search:1}"
+
+
+function getuserid 
+{	
+	records=$(cut -d: -f1 db.dat | grep -n "$search" | cut -d: -f1)
+}
 function getaddress
 {
 	records=$(cut -d: -f2 db.dat | grep -n "$search" | cut -d: -f1)
-	#for debuging
-	#echo $recordsi
 }
-
 function getphone
 {
-records=$(cut -d: -f3 db.dat | grep -n "$search" | cut -d: -f1)
-	#for debuging
-	#echo $recordsi
+	records=$(cut -d: -f3 db.dat | grep -n "$search" | cut -d: -f1)
 }
 
 function getemail
 {
 	records=$(cut -d: -f1 db.dat | grep -n "$search" | cut -d: -f1)
-	#for debuging
-	#echo $recordsi
 }
 
 
 #searches the appropriate colums
 if [[ "$1" = "u" ]] ; then 
-#if [ $1 == "user" -o $1 -eq 1 ] then 
 	getuserid
 	
 elif [[ "$1" = "a" ]] ; then
-#elif [ $1 == "address" -o $1 -eq 2 ] then
 	getaddress
 
 elif [[ "$1" = "p" ]] ; then
-#elif [ $1 == "phone" -o $1 -eq 3 ]; then
 	getphone
 
 elif [[ $1 = "e" ]] ; then
-#elif [ $1 == "email" -o $1 -eq 4 ]; then
 	getemail
 	
 else
 	echo "Select 'user' 'address' 'phone' 'email'"	
 
 fi
- 
+
+
 ##final echo
 #if no record exists
-if [ -z $records ]; then
+if [ -z "$records" ]; then
 	echo "No matches found"
 	exit 2
 fi
