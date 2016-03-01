43ab6850cdc556f9b5505029b370431dc5306501
Conner <clwherriges@gmail.com>
2016-03-01 11:44:09 -0600

    added exit codes and error checking for 0 input

--- add.sh
+++ add.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# make sure file exists and is
-# writeable before trying to add to it
+
 if [ -f db.dat ] && [ -w db.dat ]
 then
     echo "Adding a new record"
@@ -11,17 +10,44 @@ then
     do
         echo -n "Enter the name you wish to add followed by [Enter]"
         read name
-        echo -n "Enter their address followed by [Enter]"
+        if [[ -z "$name" ]]
+        then
+            printf "No input"
+            exit 1
+        fi
+
+        echo -n "Enter the address followed by [Enter]"
         read address
-        echo -n "Enter their phone number followe by [Enter]"
+        if [[ -z "$address" ]]
+        then
+            echo "No input"
+            exit 1
+        fi
+
+        echo -n "Enter the phone number followed by [Enter]"
         read phone
-        echo -n "Enter their email address followed by [Enter]"
+        if [[ -z "$phone" ]]
+        then
+            echo "No input"
+            exit 1
+        fi
+
+        echo -n "Enter the email address followed by [Enter]"
         read email
+        if [[ -z "$email" ]]
+        then
+            echo "No input"
+            exit 1
+        fi
+
         echo "$name:$address:$phone:$email" >> db.dat
         echo "Do you wish to add another contact?"
         echo -n "Press any key to continue or 'q' to quit "
         read response
     done
+    exit 0
+
 else
     echo "file does not exist"
+    exit 1
 fi
